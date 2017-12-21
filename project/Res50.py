bf75c24f9936ee38c9c65fee686598033425803b
Aydin Ayanzadeh <a.ayanzadeh@gmail.com>
2017-12-21 16:09:26 +0300

    Add files via upload

---
+++ project/Res50.py
@@ -0,0 +1,271 @@
+# -*- coding: utf-8 -*-
+import tensorflow as tf
+from keras.backend.tensorflow_backend import set_session
+config = tf.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.3
+#config.gpu_options.visible_device_list = "0"
+set_session(tf.Session(config=config))
+
+from keras.models import Sequential
+from keras.optimizers import SGD
+from keras.layers import Input, Dense, Convolution2D, MaxPooling2D, AveragePooling2D, ZeroPadding2D, Dropout, Flatten, merge, Reshape, Activation
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+from keras import backend as K
+
+from sklearn.metrics import log_loss
+
+from keras.optimizers import SGD
+from keras.layers import Input, merge, ZeroPadding2D
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers.convolutional import Convolution2D
+from keras.layers.pooling import AveragePooling2D, GlobalAveragePooling2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.models import Model
+import keras.backend as K
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import log_loss
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import keras
+from keras.applications.vgg19 import VGG19
+from keras.models import Model
+from keras.layers import Dense, Dropout, Flatten
+
+import os
+from tqdm import tqdm
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+import cv2
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+from custom_layers.scale_layer import Scale
+
+from subprocess import check_output
+
+
+def identity_block(input_tensor, kernel_size, filters, stage, block):
+    """
+    The identity_block is the block that has no conv layer at shortcut
+    Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    """
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Convolution2D(nb_filter1, 1, 1, name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter2, kernel_size, kernel_size,
+                      border_mode='same', name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter3, 1, 1, name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    x = merge([x, input_tensor], mode='sum')
+    x = Activation('relu')(x)
+    return x
+
+def conv_block(input_tensor, kernel_size, filters, stage, block, strides=(2, 2)):
+    """
+    conv_block is the block that has a conv layer at shortcut
+    # Arguments
+        input_tensor: input tensor
+        kernel_size: defualt 3, the kernel size of middle conv layer at main path
+        filters: list of integers, the nb_filters of 3 conv layer at main path
+        stage: integer, current stage label, used for generating layer names
+        block: 'a','b'..., current block label, used for generating layer names
+    Note that from stage 3, the first conv layer at main path is with subsample=(2,2)
+    And the shortcut should have subsample=(2,2) as well
+    """
+
+    nb_filter1, nb_filter2, nb_filter3 = filters
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = Convolution2D(nb_filter1, 1, 1, subsample=strides,
+                      name=conv_name_base + '2a')(input_tensor)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter2, kernel_size, kernel_size, border_mode='same',
+                      name=conv_name_base + '2b')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = Convolution2D(nb_filter3, 1, 1, name=conv_name_base + '2c')(x)
+    x = BatchNormalization(axis=bn_axis, name=bn_name_base + '2c')(x)
+
+    shortcut = Convolution2D(nb_filter3, 1, 1, subsample=strides,
+                             name=conv_name_base + '1')(input_tensor)
+    shortcut = BatchNormalization(axis=bn_axis, name=bn_name_base + '1')(shortcut)
+
+    x = merge([x, shortcut], mode='sum')
+    x = Activation('relu')(x)
+    return x
+
+def resnet50_model(img_rows, img_cols, color_type=1, num_classes=None):
+    """
+    Resnet 50 Model for Keras
+    Model Schema is based on 
+    https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+    ImageNet Pretrained Weights 
+    https://github.com/fchollet/deep-learning-models/releases/download/v0.2/resnet50_weights_th_dim_ordering_th_kernels.h5
+    Parameters:
+      img_rows, img_cols - resolution of inputs
+      channel - 1 for grayscale, 3 for color 
+      num_classes - number of class labels for our classification task
+    """
+
+    # Handle Dimension Ordering for different backends
+    global bn_axis
+    if K.image_dim_ordering() == 'tf':
+      bn_axis = 3
+      img_input = Input(shape=(img_rows, img_cols, color_type))
+    else:
+      bn_axis = 1
+      img_input = Input(shape=(color_type, img_rows, img_cols))
+
+    x = ZeroPadding2D((3, 3))(img_input)
+    x = Convolution2D(64, 7, 7, subsample=(2, 2), name='conv1')(x)
+    x = BatchNormalization(axis=bn_axis, name='bn_conv1')(x)
+    x = Activation('relu')(x)
+    x = MaxPooling2D((3, 3), strides=(2, 2))(x)
+
+    x = conv_block(x, 3, [64, 64, 256], stage=2, block='a', strides=(1, 1))
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='b')
+    x = identity_block(x, 3, [64, 64, 256], stage=2, block='c')
+
+    x = conv_block(x, 3, [128, 128, 512], stage=3, block='a')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='b')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='c')
+    x = identity_block(x, 3, [128, 128, 512], stage=3, block='d')
+
+    x = conv_block(x, 3, [256, 256, 1024], stage=4, block='a')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='b')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='c')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='d')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='e')
+    x = identity_block(x, 3, [256, 256, 1024], stage=4, block='f')
+
+    x = conv_block(x, 3, [512, 512, 2048], stage=5, block='a')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='b')
+    x = identity_block(x, 3, [512, 512, 2048], stage=5, block='c')
+
+    # Fully Connected Softmax Layer
+    x_fc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_fc = Flatten()(x_fc)
+    x_fc = Dense(1000, activation='softmax', name='fc1000')(x_fc)
+
+    # Create model
+    model = Model(img_input, x_fc)
+
+    # Load ImageNet pre-trained data 
+    if K.image_dim_ordering() == 'th':
+      # Use pre-trained weights for Theano backend
+      weights_path = 'imagenet_models/resnet50_weights_th_dim_ordering_th_kernels.h5'
+    else:
+      # Use pre-trained weights for Tensorflow backend
+      weights_path = 'imagenet_models/resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+
+    model.load_weights(weights_path)
+
+        
+    for layer in model.layers[:-2]:
+        layer.trainable = False
+    # Truncate and replace softmax layer for transfer learning
+    # Cannot use model.layers.pop() since model is not of Sequential() type
+    # The method below works since pre-trained weights are stored in layers but not in the model
+    x_newfc = AveragePooling2D((7, 7), name='avg_pool')(x)
+    x_newfc = Flatten()(x_newfc)
+    x_newfc = Dense(num_classes, activation='softmax', name='fc10')(x_newfc)
+
+    # Create another model with our customized softmax
+    model = Model(img_input, x_newfc)
+
+    # Learning rate is changed to 0.001
+    sgd = SGD(lr=1e-3, decay=1e-6, momentum=0.9, nesterov=True)
+    model.compile(optimizer=sgd, loss='categorical_crossentropy', metrics=['accuracy'])
+  
+    return model
+
+if __name__ == '__main__':
+
+    # Example to fine-tune on 3000 samples from Cifar10
+
+    img_rows, img_cols = 224, 224 # Resolution of inputs
+    channel = 3
+    num_classes = 120 
+    batch_size = 16 
+    nb_epoch = 250
+    
+    directory_path = "/home/aydin/storage/aydin/kaggle-dog/Kaggle-dog-breeds-classification/cnn_finetune-master/"
+
+    print(check_output(["ls", directory_path]).decode("utf8"))
+
+    df_train = pd.read_csv(directory_path+'labels.csv')
+    df_test = pd.read_csv(directory_path+'sample_submission.csv')
+
+    targets_series = pd.Series(df_train['breed'])
+    one_hot = pd.get_dummies(targets_series, sparse = True)
+
+    one_hot_labels = np.asarray(one_hot)
+
+    im_size = 224
+    x_train = []
+    y_train = []
+    x_test = []
+  
+
+    i = 0 
+    for f, breed in tqdm(df_train.values):
+        img = cv2.imread(directory_path+'train/{}.jpg'.format(f))
+        label = one_hot_labels[i]
+        x_train.append(cv2.resize(img, (im_size, im_size)))
+        y_train.append(label)
+        i += 1
+
+
+    for f in tqdm(df_test['id'].values):
+        img = cv2.imread(directory_path+'test/{}.jpg'.format(f))
+        x_test.append(cv2.resize(img, (im_size, im_size)))
+
+    y_train_raw = np.array(y_train, np.uint8)
+    x_train_raw = np.array(x_train, np.float32) / 255.
+    x_test  = np.array(x_test, np.float32) / 255.
+
+    print(x_train_raw.shape)
+    print(y_train_raw.shape)
+    print(x_test.shape)
+
+    num_class = y_train_raw.shape[1]
+
+
+    # Load Cifar10 data. Please implement your own load_data() module for your own dataset
+    X_train, X_valid, Y_train, Y_valid = train_test_split(x_train_raw, y_train_raw, test_size=0.1, random_state=1)
+
+    # Load our model
+    model = resnet50_model(img_rows, img_cols, channel, num_classes)
+
+    # Start Fine-tuning
+    model.fit(X_train, Y_train,
+              batch_size=batch_size,
+              nb_epoch=nb_epoch,
+              shuffle=True,
+              verbose=1,
+              validation_data=(X_valid, Y_valid),
+              )
+
+    # Make predictions
+    predictions_valid = model.predict(X_valid, batch_size=batch_size, verbose=1)
+
+    # Cross-entropy loss score
+    score = log_loss(Y_valid, predictions_valid)
---
+++ project/readme.txt
@@ -0,0 +1,8 @@
+we implemented 4 models for this project, googleNet, ResNet50, DensNet121,DensNet169
+
+1-Densnet169.py
+2-densnet-121.py
+3-ResNet.py
+4-GoogleNet.py
+
+
